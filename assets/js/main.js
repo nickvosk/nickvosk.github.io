4ecceb04318b7f418a8840f81f24c28e4b52b0ee
Nikos Voskarides <n.voskarides@uva.nl>
2020-05-26 18:35:49 +0300

    security fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,10 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+jQuery.htmlPrefilter = function( html ) {
+	return html;
+};
+
 (function($) {
 
 	skel.breakpoints({
